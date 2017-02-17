4acb7434e4571b286940dad58f67f6d50c65ecc8
Paul Wade <paul.wade@live.com>
2017-02-17 09:03:25 -0600

    init novi code

---
+++ src/CoasterQuery/CoasterQuery/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="CoasterQuery.MvcApplication" Language="C#" %>
---
+++ src/CoasterQuery/CoasterQuery/Repositories/AmusementParkRepository.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CoasterQuery.Repositories
+{
+	public class AmusementParkRepository
+	{
+		// This endpoint is an API end (JSON or XML) with all amusement parks:
+		// GET https://coasterbuzz.com/api/park
+		// This endpoint uses the ParkID from the above entries to get just a single park (replace {id} with ID):
+		// GET https://coasterbuzz.com/api/park/{id}
+	}
+}
