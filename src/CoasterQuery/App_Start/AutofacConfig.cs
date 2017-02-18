ec78530736fecd2f31b2c17602e84b1b0201c946
Paul Wade <paul.wade@live.com>
2017-02-18 15:42:15 -0600

    pull out DI registrations and put them in the right place.

---
+++ src/CoasterQuery/App_Start/AutofacConfig.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Web;
+using System.Web.Http;
+using Autofac;
+using Autofac.Integration.WebApi;
+using CoasterQuery.Business.Services;
+using CoasterQuery.Data.Models;
+using CoasterQuery.Data.Repositories;
+
+namespace CoasterQuery.App_Start
+{
+    public class AutofacConfig
+    {
+        public static void Register(HttpConfiguration config)
+        {
+            var builder = new ContainerBuilder();
+
+            // Register your Web API controllers.
+            builder.RegisterApiControllers(Assembly.GetExecutingAssembly());
+
+            //app registrations.
+            builder.RegisterType<AmusementParkRepository>().As<IRepository<Park>>();
+            builder.RegisterType<ParkService>().As<IParkService>();
+            builder.RegisterType<MappingService>().As<IMappingService>();
+
+            // Set the dependency resolver to be Autofac.
+            var container = builder.Build();
+            config.DependencyResolver = new AutofacWebApiDependencyResolver(container);
+        }
+    }
+}
--- src/CoasterQuery/Global.asax.cs
+++ src/CoasterQuery/Global.asax.cs
@@ -23,23 +23,11 @@ namespace CoasterQuery
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
 
-            var builder = new ContainerBuilder();
-
             // Get your HttpConfiguration.
-            var config = GlobalConfiguration.Configuration; 
+            var config = GlobalConfiguration.Configuration;
             config.EnableCors();
-            // Register your Web API controllers.
-            builder.RegisterApiControllers(Assembly.GetExecutingAssembly());
- 
-            //my registrations
-            //TODO move builder out of global.
-            builder.RegisterType<AmusementParkRepository>().As<IRepository<Park>>();
-            builder.RegisterType<ParkService>().As<IParkService>();
-            builder.RegisterType<MappingService>().As<IMappingService>();
-            // Set the dependency resolver to be Autofac.
-            var container = builder.Build();
-            config.DependencyResolver = new AutofacWebApiDependencyResolver(container);
-             
+            AutofacConfig.Register(config);
+
         }
     }
 }
